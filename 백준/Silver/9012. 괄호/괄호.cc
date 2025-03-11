d62146d16bf5a1ac40c9e032aa8fb222c576a266
wumpusu <lewnskms@naver.com>
2025-03-11 22:25:20 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	
+	cin >> t;
+	
+	while(t--){
+		stack<char> st;
+		string s;
+		
+		cin >> s;
+		
+		for(int i=0;i<s.length();i++){
+			if(st.empty()){
+				st.push(s[i]);
+			}
+			else{
+				if(st.top() == '(' and s[i] == ')' ){
+					st.pop();
+				}
+				
+				else{
+					st.push(s[i]);
+				}
+			}
+		
+		}
+		
+		if(st.empty()){
+			cout << "YES" << '\n';
+		}
+		else{
+			cout << "NO" << '\n';
+		}
+		
+	}
+
+}

b0e1df6ff55551a98e705ffe107d854fecf45f65
wumpusu <lewnskms@naver.com>
2025-03-20 21:46:29 +0900

    [Silver I] Title: 포도주 시식, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  int n;
+  
+  cin >> n;
+  int arr[10001];
+  int dp[10001];
+  
+  for(int i=1;i<=n;i++){
+    cin >> arr[i];
+  }
+   
+   dp[1] = arr[1];
+    if(n>1){
+        dp[2] = arr[1] + arr[2];
+    }
+
+    
+    for(int i=3;i<=n;i++){
+        dp[i] = max({dp[i-1],dp[i-2]+arr[i] , dp[i-3]+arr[i-1]+arr[i]});
+    }
+    
+    cout << dp[n];
+  
+}

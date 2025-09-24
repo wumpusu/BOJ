d386814ffce159377ad0a17b20b3fc42c2d1176c
wumpusu <lewnskms@naver.com>
2025-09-24 12:00:53 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10171. 고양이/고양이.cc
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -1,10 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
- 
-int main(int argc, char const *argv[]) {
-    printf("\\    /\\\n");
-    printf(" )  ( ')\n");
-    printf("(  /  )\n");
-    printf(" \\(__)|\n");
-    return 0;
+
+int main() 
+{
+    cout<<"\\    /\\"<<"\n";
+    cout<<" )  ( ')"<<"\n";
+    cout<<"(  /  )"<<"\n";
+    cout<<" \\(__)|";
+    
 }
